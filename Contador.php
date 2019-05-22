bc0b674ed80663d889931e20e118beb7aae35029
Sara Ramírez Chávez <46709918+saralgh@users.noreply.github.com>
2019-05-22 11:10:06 +0200

    Add files via upload

---
+++ Contador.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+    <link href="countvisiststyle.css" rel="stylesheet">
+    <?php
+        include("countfunction.php");
+    ?>
+</head>
+<body>
+    <?php
+        ShowVisits()
+    ?>
+</body>
+</html>
---
+++ countfunction.php
@@ -0,0 +1,23 @@
+<?php
+    function ShowVisits()
+    {
+        //Introduce una nueva visita
+        $fcvroute="C:/Contador/CountVisits.txt";
+        $fcv=fopen($fcvroute,"r");
+        $num=fread($fcv, filesize($fcvroute));
+        $num++;
+        echo($num);
+        $arrayvisits=str_split($num,1); //Convierte una cadena de texto en array.
+        foreach($arrayvisits as $numv)
+        {
+            echo("<div>");
+            echo($numv);
+            echo("</div>");
+        }
+        fclose($fcv);
+        
+        $fcvl=fopen($fcvroute,"w+");
+        fwrite($fcvl,$num);
+        fclose($fcvl);
+    }
+?>
